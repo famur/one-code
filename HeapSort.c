db5308c60e59f9b1d6af3ce703e2c186fed0acfe
famur <1779936164@qq.com>
2019-10-03 05:07:08 -0700

    second add code

--- HeapSort.c
+++ HeapSort.c
@@ -1,31 +1,63 @@
 #include<stdio.h>
 
-void Justdown(int *parray, int n, int p)
+void Swap(int *a,int *b)
 {
-    int i = 0;
-    int child = i * 2 + 1;
+    int p = 0;
+    p = *b;
+    *b = *a;
+    *a = p;
+}
+
+void Justdown(int *a, int n, int parent)
+{
+    
+    int child = parent * 2 + 1;
+    int temp = 0;
     while(child < n)
     {
-        int child = i * 2 + 1;
-        if((child + 1 < n) && parray[child + 1] > parray[child])
+        if((child + 1 < n) && a[child + 1] > a[child])
         {
             ++child;
         }
-        if(parray[child] > parray[i])
+        if(a[child] > a[parent])
         {
-            int max = 0;
-            max = parray[child];
-            parray[child] = parray[i];
-            parray[i] = max;
+            Swap(&a[child], &a[parent]);
+            parent = child;
+            child = parent * 2 + 1;
         }
         else
         {
             break;
         }
-        i++;
     }
+}
+
+void HeapSort(int *a, int n)
+{
+    int t = 0,i = 0;
+    int end = n - 1;
+    for (i = (n - 2) / 2; i >= 0; i--)
+    {
+        Justdown(a, n, i);
+    }
+    while(end > 0)
+    {
+        Swap(&a[end], &a[0]);
+        Justdown(a, end, 0);
+        end--;
+    }
+    for(t = 0; t < n; t++)
+    {
+        printf("%d",a[t]);
+    }
+    printf("\n");
+
+
 }
 int main()
 {
+    int array[] = {5,1,6,3,7,9,3,4,5};
+    int length = sizeof(array) / sizeof(array[0]);
+    HeapSort(array,length);
     return 0;
 }
